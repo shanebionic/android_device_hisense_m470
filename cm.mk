87bc98de3e55486a22d25b5b9ec73b9faa25dba9
Kevin Davis <dumbandroid@gmail.com>
2013-10-06 21:18:55 -0700

    build torch
    Change-Id: Ieac959194dfb94f8be11613529445fdbbd49dc72

--- cm.mk
+++ cm.mk
@@ -23,3 +23,6 @@ PRODUCT_MANUFACTURER := Hisense
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m470 BUILD_FINGERPRINT="google/nakasi/grouper:4.3/JWR66V/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.3 JWR66V 573038 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
