1775764631bec1ce88393853da89207b7f174543
randomblame <dumbandroid@gmail.com>
2013-10-28 18:47:40 -0700

    remove junk and fixup aosp building
    Change-Id: Ic875aec5267f4e59aaafc32bd598614953e1e4b4

--- full_m470.mk
+++ full_m470.mk
@@ -21,14 +21,18 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/hisense/m470/device.mk)
+
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_sero7pro
-PRODUCT_DEVICE := sero7pro
+PRODUCT_NAME := full_m470
+PRODUCT_DEVICE := m470
 PRODUCT_BRAND := Hisense
 # Don't restrict vendor folder
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_MODEL := AOSP on Sero7pro
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m470 BUILD_FINGERPRINT="google/nakasi/grouper:4.3/JWR66V/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.3 JWR66V 573038 release-keys"
