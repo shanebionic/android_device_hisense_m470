37c0627e16b9a8929f670a15e526d34d064a4a2b
randomblame <dumbandroid@gmail.com>
2013-12-16 19:28:17 -0800

    clean out boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/hisense/m470/bluetooth
 
 TARGET_BOARD_PLATFORM := tegra3
 TARGET_TEGRA_VERSION := t30
@@ -25,27 +24,25 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
 TARGET_USE_O3 := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_NO_BOOTLOADER := true
 
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 681574400
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 6567231488
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-USE_CAMERA_STUB := false
-
-USE_PROPRIETARY_AUDIO_EXTENSIONS := true
 BOARD_LIB_DUMPSTATE := libdumpstate.m470
-TARGET_RECOVERY_UI_LIB := librecovery_ui_m470
-RECOVERY_FSTAB_VERSION := 2
 
--include vendor/hisense/m470/BoardConfigVendor.mk
-
-TARGET_RECOVERY_FSTAB = device/hisense/m470/prebuilt/ramdisk/fstab.m470
-BOARD_RECOVERY_SWIPE := true
+# Kernel
 TARGET_PREBUILT_KERNEL := device/hisense/m470/prebuilt/kernel/kernel
 
-# Fix camera and load all nvlibs properly
-BOARD_EGL_NEEDS_LEGACY_FB := true
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/hisense/m470/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Wireless
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
@@ -62,26 +59,27 @@ WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcmdhd_apsta.bin
 WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/system/etc/nvram.txt iface_name=wlan0"
 
 # Sensors invensense
-BOARD_USES_GENERIC_INVENSENSE := true
-
-TARGET_NO_BOOTLOADER := true
-
-BOARD_HARDWARE_CLASS := device/hisense/m470/cmhw/
-
-COMMON_GLOBAL_CFLAGS += -DHAVE_PRE_KITKAT_AUDIO_BLOB
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
 BOARD_USES_GENERIC_INVENSENSE := false
 
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
+# Audio Options
+USE_PROPRIETARY_AUDIO_EXTENSIONS := true
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+BOARD_USES_TINY_AUDIO_HW := false
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
 
+# Video
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/hisense/m470/prebuilt/lib/egl/egl.cfg
-
-# Avoid the generation of ldrcc instructions
-NEED_WORKAROUND_CORTEX_A9_745320 := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_UI_LIB := librecovery_ui_m470
+RECOVERY_FSTAB_VERSION := 2
 USE_SET_METADATA := false
+TARGET_RECOVERY_FSTAB = device/hisense/m470/prebuilt/ramdisk/fstab.m470
+BOARD_RECOVERY_SWIPE := true
+
+BOARD_HARDWARE_CLASS := device/hisense/m470/cmhw/
