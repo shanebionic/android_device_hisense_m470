4c282666ff61dea10edd449a1c456bcc0ba3ffb0
davepmer20102010@gmail.com <davepmer20102010@gmail.com>
2013-12-27 14:17:36 -0500

    MP3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,10 +38,10 @@ BOARD_LIB_DUMPSTATE := libdumpstate.m470
 
 # Kernel
 #TARGET_PREBUILT_KERNEL := device/hisense/m470/prebuilt/kernel/kernel
-BOARD_KERNEL_CMDLINE := androidboot.selinux=enforce
+BOARD_KERNEL_CMDLINE := androidboot.selinux=disable
 TARGET_KERNEL_SOURCE := kernel/hisense/m470
 TARGET_KERNEL_CONFIG := m470_defconfig
-# TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/hisense/m470/bluetooth
