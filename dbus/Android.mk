0290fb034c15ec76aa4a26e2fa4516b23ff6d8ae
davepmer20102010@gmail.com <davepmer20102010@gmail.com>
2014-03-11 11:45:23 -0400

    building dbus was causing reboots

--- dbus/Android.mk
+++ dbus/Android.mk
@@ -1,5 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),m470)
 
-include $(call all-subdir-makefiles)
+include $(CLEAR_VARS)
+LOCAL_MODULE := dbus-daemon
+LOCAL_SRC_FILES := dbus-daemon
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := broadcom
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdbus
+LOCAL_SRC_FILES := libdbus.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := hisense
+include $(BUILD_PREBUILT)
 
 endif
