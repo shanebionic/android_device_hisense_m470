f34e95841d90c60cb45d31d57f6c2154e0feb32f
davepmer20102010@gmail.com <davepmer20102010@gmail.com>
2014-03-26 19:30:42 -0400

    Revemay not be needed but better to have it

--- device.mk
+++ device.mk
@@ -91,10 +91,12 @@ PRODUCT_PACKAGES += \
     charger \
     charger_res_images \
     com.android.future.usb.accessory \
+    dbus-daemon \
     keystore.m470 \
     l2ping \
     libaudioutils \
     libbt-vendor \
+    libdbus \
     libdumpstate.m470 \
     libhealthd.m470 \
     librs_jni \
